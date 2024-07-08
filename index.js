1b08b5d010d2afaf5a83ea8056d17c6480a9c5a5
Sindhumandadapu <sindhumandadapu24@gmail.com>
2024-07-08 11:48:28 +0530

    changes

--- index.js
+++ index.js
@@ -18,15 +18,11 @@ mongoose.connect(url)
 const server = new ApolloServer({ typeDefs, resolvers });
 
 async function startServer() {
-    await server.start();
-    server.applyMiddleware({ app });
-
-    app.listen(port, () => {
-        console.log(`Server is live on port ${port}`);
-    });
+   
 }
+
 function Testing(){
-    return 0;
+    return 1;
 }
-
-startServer();
+Testing();
+StartServer();
